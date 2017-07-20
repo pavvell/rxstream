83dd062d03f4e8476030fb22919fc8e8afbf5471
Pavel Nesmelov <pavel.nesmelov@piano.io>
2017-07-20 15:01:30 +0400

    +merge subjects tests

--- app/index.js
+++ app/index.js
@@ -1,13 +1,21 @@
 import Subject from '../lib/subjects/subject';
+import BehaviorSubject from '../lib/subjects/behaviourSubject';
 import merge from '../lib/operators/merge';
 
 let first$ = new Subject();
-let second$ = new Subject();
-let merged$ = merge(Subject, [first$, second$]);
+let second$ = new BehaviorSubject();
 
+second$.next(0);
+
+let merged$ = merge(Subject, [first$, second$]);
 let received = [];
 
-merged$.subscribe(value => received.push(value));
+second$.subscribe(value => console.log('direct subscribe...', value));
+
+merged$.subscribe(value => {
+  received.push(value);
+  console.log('received...', value);
+});
 
 first$.next(1);
 second$.next(2);
--- tests/operators/operators.js
+++ tests/operators/operators.js
@@ -1,12 +1,17 @@
 import Subject from '../../lib/subjects/subject';
+import BehaviorSubject from '../../lib/subjects/behaviourSubject';
+import ReplaySubject from '../../lib/subjects/replaySubject';
 import merge from '../../lib/operators/merge';
 
 describe("operators", function() {
 
   describe("merge", function() {
-    it("should merge Subjects in one stream", function(done) {
+    it("should merge different streams in one stream of type Subject", function(done) {
       let first$ = new Subject();
-      let second$ = new Subject();
+      let second$ = new BehaviorSubject();
+
+      second$.next(0);
+
       let merged$ = merge(Subject, [first$, second$]);
 
       let received = [];
@@ -17,6 +22,55 @@ describe("operators", function() {
       second$.next(2);
 
       assert.deepEqual(received, [1, 2]);
+      done();
+    });
+
+    it("should merge different streams in one stream of type BehaviorSubject", function(done) {
+      let first$ = new Subject();
+      let second$ = new BehaviorSubject();
+      let third$ = new BehaviorSubject();
+
+      first$.next(1);
+      second$.next(2);
+      third$.next(3);
+
+      let merged$ = merge(BehaviorSubject, [first$, second$, third$]);
+
+      let received = [];
+
+      merged$.subscribe(value => received.push(value));
+
+      first$.next(4);
+      second$.next(5);
+      third$.next(6);
+
+      assert.deepEqual(received, [3, 4, 5, 6]);
+      done();
+    });
+
+    it("should merge different streams in one stream of type ReplaySubject", function(done) {
+      let first$ = new Subject();
+      let second$ = new BehaviorSubject();
+      let third$ = new ReplaySubject();
+
+      first$.next(1);
+      second$.next(2);
+      second$.next(3);
+      third$.next(4);
+      third$.next(5);
+
+      let merged$ = merge(ReplaySubject, [first$, second$, third$]);
+
+      let received = [];
+
+      merged$.subscribe(value => received.push(value));
+
+      first$.next(6);
+      second$.next(7);
+      third$.next(8);
+      third$.next(9);
+
+      assert.deepEqual(received, [3, 4, 5, 6, 7, 8, 9]);
       done();
     });
   });
