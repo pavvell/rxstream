286c3fcfdab4cfd07541e90147164bad847567e2
Pavel Nesmelov <pavel.nesmelov@piano.io>
2017-08-01 14:48:05 +0400

    update readme, -heroku

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: node app/heroku/index.js
--- app/heroku/index.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let path = require('path');
-let app = express();
-
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(path.join(__dirname, '../../doc')));
-
-app.listen(app.get('port'));
