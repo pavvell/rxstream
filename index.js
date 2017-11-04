0d85f6530e2f7203a4a2c6091f749c5f137a2a32
Pavel Nesmelov <pavel.nesmelov@piano.io>
2017-11-04 22:03:58 +0400

    export modules

---
+++ index.js
@@ -0,0 +1,8 @@
+export { Subject } from './es5/subjects/subject';
+export { BehaviorSubject } from './es5/subjects/behaviourSubject';
+export { ReplaySubject } from './es5/subjects/replaySubject';
+
+export { merge } from './es5/operators/merge';
+export { filter } from './es5/operators/filter';
+export { combineLatest } from './es5/operators/combineLatest';
+export { debounce } from './es5/operators/debounce';
